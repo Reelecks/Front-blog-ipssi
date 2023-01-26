172e28a59ef0554c078d25260b4f62b932c6544b
Reelecks <robinlandais75@yahoo.com>
2023-01-27 00:15:04 +0100

    add: nction and routes. login register post

---
+++ js/generate.js
@@ -0,0 +1,423 @@
+function generateSingleArticle(id) {
+  clearMain();
+  fetch(`http://127.0.0.1:4000/api/post/${id}`, {
+    method: "GET",
+    header: {
+      Authorization: document.cookie,
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const single_article = document.createElement("div");
+      single_article.classList.add("single_article");
+      single_article.style.display = "flex";
+      document.querySelector(".main").appendChild(single_article);
+      const container_article = document.createElement("div");
+      container_article.classList.add("container_article");
+      const article = document.createElement("article");
+      article.classList.add("single_article");
+      single_article.appendChild(container_article);
+      container_article.appendChild(article);
+      const single_title = document.createElement("div");
+      single_title.classList.add("single_title");
+      single_title.textContent = data.title;
+      article.appendChild(single_title);
+      article.appendChild(document.createElement("hr"));
+      const author = document.createElement("div");
+      author.classList.add("single_author");
+      const date = document.createElement("div");
+      date.classList.add("single_date");
+      const author_name = data.user.username;
+      author.textContent = `Auteur: ${author_name}`;
+      date.textContent = `Publié le : ${data.createdAt}`;
+      article.appendChild(author);
+      article.appendChild(date);
+      article.appendChild(document.createElement("hr"));
+      const text = document.createElement("div");
+      text.classList.add("single_story");
+      text.textContent = data.text;
+      container_article.appendChild(text);
+      const divButton = document.createElement("div");
+      divButton.classList.add("divButton");
+      const deleteButton = document.createElement("button");
+      deleteButton.classList.add("button");
+      deleteButton.innerText = "Supprimer";
+      deleteButton.style.backgroundColor = "red";
+      deleteButton.setAttribute("onclick", ``);
+
+      const listButton = document.createElement("button");
+      listButton.innerText = "Retour à la liste";
+      listButton.style.backgroundColor = " #422af6";
+      listButton.classList.add("button");
+      listButton.setAttribute("onclick", `generateAllArticle()`);
+      container_article.appendChild(divButton);
+      divButton.appendChild(deleteButton);
+      divButton.appendChild(listButton);
+      for (comment of data.Comments) {
+        let comment_container = document.createElement("div");
+        comment_container.classList.add("comments_container");
+
+        let comment_date = document.createElement("div");
+        comment_date.classList.add("comments_date");
+
+        let comment_text = document.createElement("div");
+        comment_text.classList.add("comments_text");
+
+        let comment_userID = document.createElement("div");
+        comment_userID.classList.add("comments_author");
+
+        let comment_infos = document.createElement("div");
+        comment_infos.classList.add("comments_infos");
+
+        let comment_fill = document.createElement("div");
+        comment_fill.classList.add("comments_fill");
+        comment_container.appendChild(comment_fill);
+        comment_fill.appendChild(comment_infos);
+        comment_fill.appendChild(comment_text);
+        comment_infos.appendChild(comment_userID);
+        comment_infos.appendChild(comment_date);
+        comment_userID.textContent = comment.user.username;
+        comment_date.textContent = comment.createdAt;
+        comment_text.textContent = comment.texte;
+        document.querySelector(".main").appendChild(comment_container);
+      }
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
+}
+
+function generateAllArticle() {
+  clearMain();
+  fetch(`http://127.0.0.1:4000/api/post/all`, {
+    method: "GET",
+    header: {
+      Authorization: document.cookie,
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Create the main container div
+      const mainDiv = document.createElement("div");
+      mainDiv.classList.add("container", "accueil_articles");
+      mainDiv.style.display = "grid";
+      document.querySelector(".main").appendChild(mainDiv);
+
+      // Create the h1 element
+      const h1 = document.createElement("h1");
+      h1.classList.add("center");
+      h1.innerText = "Les articles";
+
+      // Append the h1 to the main div
+      mainDiv.appendChild(h1);
+      for (articleItem of data) {
+        // Create the article element
+        const article = document.createElement("article");
+        article.classList.add("episode");
+
+        // Create the episode number div
+        const episodeNumber = document.createElement("div");
+        episodeNumber.classList.add("episode__number");
+        episodeNumber.innerText = articleItem.title;
+
+        // Create the episode content div
+        const episodeContent = document.createElement("div");
+        episodeContent.classList.add("episode__content");
+
+        // Create the title div
+        const title = document.createElement("div");
+        title.classList.add("title");
+        title.innerHTML = `${articleItem.createdAt} <br /> ${articleItem.user.username}`;
+
+        // Create the story div
+        const story = document.createElement("div");
+        story.classList.add("story");
+
+        // Create the story paragraphs
+        const storyParagraph1 = document.createElement("p");
+        storyParagraph1.innerText = articleItem.text;
+        // Append the story paragraphs to the story div
+        story.appendChild(storyParagraph1);
+        // Create the button element
+        const button = document.createElement("button");
+        button.classList.add("button");
+        button.innerText = "Page de l'article";
+        button.setAttribute(
+          "onclick",
+          `generateSingleArticle('${articleItem.id}')`
+        );
+        // Append the title and story divs to the episode content div
+        episodeContent.appendChild(title);
+        episodeContent.appendChild(story);
+
+        // Append the episode number, episode content and button to the article element
+        article.appendChild(episodeNumber);
+        article.appendChild(episodeContent);
+        article.appendChild(button);
+
+        // Append the article to the main div
+        mainDiv.appendChild(article);
+      }
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
+}
+function deleteArticle() {}
+function generateLoginForm() {
+  clearMain()
+  let mainLog = document.createElement("div");
+  mainLog.classList.add("main_log");
+
+  let logContainer = document.createElement("div");
+  logContainer.classList.add("log_container");
+  mainLog.appendChild(logContainer);
+
+  let loginContainer = document.createElement("div");
+  loginContainer.classList.add("login_container", "form_container");
+  logContainer.appendChild(loginContainer);
+
+  let form = document.createElement("form");
+  form.setAttribute("action", "");
+  form.setAttribute("id", "connectForm");
+  loginContainer.appendChild(form);
+
+  let h2 = document.createElement("h2");
+  h2.textContent = "Connexion";
+  form.appendChild(h2);
+
+  let formRow1 = document.createElement("div");
+  formRow1.classList.add("form-row");
+  form.appendChild(formRow1);
+
+  let label1 = document.createElement("label");
+  label1.setAttribute("for", "username");
+  label1.textContent = "User Name";
+  formRow1.appendChild(label1);
+
+  let input1 = document.createElement("input");
+  input1.setAttribute("type", "text");
+  input1.classList.add("input-text", "input-text-block", "w-100");
+  input1.setAttribute("id", "username");
+  input1.setAttribute("name", "username");
+  formRow1.appendChild(input1);
+
+  let formRow2 = document.createElement("div");
+  formRow2.classList.add("form-row");
+  form.appendChild(formRow2);
+
+  let label2 = document.createElement("label");
+  label2.setAttribute("for", "password");
+  label2.textContent = "Password";
+  formRow2.appendChild(label2);
+
+  let input2 = document.createElement("input");
+  input2.setAttribute("type", "password");
+  input2.classList.add("input-text", "input-text-block", "w-100");
+  input2.setAttribute("id", "password");
+  input2.setAttribute("name", "password");
+  formRow2.appendChild(input2);
+
+  let formRow3 = document.createElement("div");
+  formRow3.classList.add("form-row", "mx-auto");
+  form.appendChild(formRow3);
+
+  let button = document.createElement("button");
+  button.setAttribute("type", "submit");
+  button.classList.add("btn-submit");
+  button.setAttribute("id", "btnSubmit");
+  button.textContent = "Submit";
+  formRow3.appendChild(button);
+
+  let main = document.querySelector(".main");
+  main.appendChild(mainLog);
+
+  const connectForm = document.querySelector("#connectForm");
+  if (connectForm) {
+    connectForm.addEventListener("submit", function (e) {
+      e.preventDefault();
+      submitFormConnect(e, this);
+    });
+  }
+}
+
+function generateRegisterForm() {
+  clearMain()
+  const mainLog = document.createElement("div");
+  mainLog.classList.add("main_log");
+
+  const logContainer = document.createElement("div");
+  logContainer.classList.add("log_container");
+  mainLog.appendChild(logContainer);
+
+  const loginContainer = document.createElement("div");
+  loginContainer.classList.add("login_container", "form_container");
+  logContainer.appendChild(loginContainer);
+
+  const form = document.createElement("form");
+  form.setAttribute("action", "");
+  form.setAttribute("id", "registerForm");
+  loginContainer.appendChild(form);
+
+  const h2 = document.createElement("h2");
+  h2.textContent = "Inscription";
+  form.appendChild(h2);
+
+  const formRow1 = document.createElement("div");
+  formRow1.classList.add("form-row");
+  form.appendChild(formRow1);
+
+  const label1 = document.createElement("label");
+  label1.setAttribute("for", "username");
+  label1.textContent = "User Name";
+  formRow1.appendChild(label1);
+
+  const input1 = document.createElement("input");
+  input1.setAttribute("type", "text");
+  input1.classList.add("input-text", "input-text-block", "w-100");
+  input1.setAttribute("id", "username");
+  input1.setAttribute("name", "username");
+  formRow1.appendChild(input1);
+
+  const formRow2 = document.createElement("div");
+  formRow2.classList.add("form-row");
+  form.appendChild(formRow2);
+
+  const label2 = document.createElement("label");
+  label2.setAttribute("for", "password");
+  label2.textContent = "Password";
+  formRow2.appendChild(label2);
+
+  const input2 = document.createElement("input");
+  input2.setAttribute("type", "password");
+  input2.classList.add("input-text", "input-text-block", "w-100");
+  input2.setAttribute("id", "password");
+  input2.setAttribute("name", "password");
+  formRow2.appendChild(input2);
+
+  const formRow3 = document.createElement("div");
+  formRow3.classList.add("form-row");
+  form.appendChild(formRow3);
+
+  const label3 = document.createElement("label");
+  label3.setAttribute("for", "role");
+  label3.textContent = "Role";
+  formRow3.appendChild(label3);
+
+  const input3 = document.createElement("input");
+  input3.setAttribute("type", "text");
+  input3.classList.add("input-text", "input-text-block", "w-100");
+  input3.setAttribute("id", "role");
+  input3.setAttribute("name", "role");
+  formRow3.appendChild(input3);
+
+  const formRow4 = document.createElement("div");
+  formRow4.classList.add("form-row", "mx-auto");
+  form.appendChild(formRow4);
+
+  const button = document.createElement("button");
+  button.setAttribute("type", "submit");
+  button.classList.add("btn-submit");
+  button.setAttribute("id", "btnSubmit");
+  button.textContent = "Submit";
+  formRow4.appendChild(button);
+
+  let main = document.querySelector(".main");
+  main.appendChild(mainLog);
+  const registerForm = document.querySelector("#registerForm");
+  if (registerForm) {
+    registerForm.addEventListener("submit", function (e) {
+      submitFormRegister(e, this);
+    });
+  }
+}
+
+function generateNewArticle() {
+  clearMain()
+  const mainNewArticle = document.createElement("div");
+  mainNewArticle.classList.add("main_newArticle");
+
+  const newArticleContainer = document.createElement("div");
+  newArticleContainer.classList.add("newArticle_container");
+  newArticleContainer.style.margin='2em 10em 2em 10em'
+  mainNewArticle.appendChild(newArticleContainer);
+
+  const articleContainer = document.createElement("div");
+  articleContainer.classList.add("article_container", "form_container");
+  newArticleContainer.appendChild(articleContainer);
+
+  const form = document.createElement("form");
+  form.setAttribute("action", "");
+  form.setAttribute("id", "newArticleForm");
+  articleContainer.appendChild(form);
+
+  const h2 = document.createElement("h2");
+  h2.textContent = "Nouvel Article";
+  form.appendChild(h2);
+
+  const formRow1 = document.createElement("div");
+  formRow1.classList.add("form-row");
+  form.appendChild(formRow1);
+
+  const label1 = document.createElement("label");
+  label1.setAttribute("for", "title");
+  label1.textContent = "Titre";
+  formRow1.appendChild(label1);
+
+  const input1 = document.createElement("input");
+  input1.setAttribute("type", "text");
+  input1.classList.add("input-text", "input-text-block", "w-100");
+  input1.setAttribute("id", "title");
+  input1.style.marginBottom ='2em'
+  input1.setAttribute("name", "title");
+  formRow1.appendChild(input1);
+
+  const formRow2 = document.createElement("div");
+  formRow2.classList.add("form-row");
+  form.appendChild(formRow2);
+
+  const label2 = document.createElement("label");
+  label2.setAttribute("for", "text");
+  label2.textContent = "Texte de L'article";
+  formRow2.appendChild(label2);
+
+  const input2 = document.createElement("textarea");
+  input2.setAttribute("type", "text");
+  input2.classList.add("input-text", "input-text-block", "w-100");
+  input2.setAttribute("id", "text");
+  input2.setAttribute("name", "text");
+  input2.style.minHeight='15em'
+  formRow2.appendChild(input2);
+
+  const formRow3 = document.createElement("div");
+  formRow3.classList.add("form-row", "mx-auto");
+  form.appendChild(formRow3);
+
+  const button = document.createElement("button");
+  button.setAttribute("type", "submit");
+  button.classList.add("btn-submit");
+  button.setAttribute("id", "btnSubmit");
+  button.textContent = "Submit";
+  formRow3.appendChild(button);
+
+  let main = document.querySelector(".main");
+  main.appendChild(mainNewArticle);
+  const registerForm = document.querySelector("#newArticleForm");
+  if (registerForm) {
+    registerForm.addEventListener("submit", function (e) {
+      submitFormNewArticle(e, this);
+    });
+  }
+}
+
+function clearMain() {
+  const main = document.querySelector(".main");
+  while (main.firstChild) {
+    main.removeChild(main.firstChild);
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,127 +1,111 @@
 /*--Functions--*/
 async function submitFormConnect(e, form) {
-    // 1. Prevent reloading page
-    e.preventDefault();
-    // 2. Submit the form
-    // 2.1 User Interaction
-    const btnSubmit = document.getElementById('btnSubmit');
-    btnSubmit.disabled = true;
-    setTimeout(() => btnSubmit.disabled = false, 2000);
-    // 2.2 Build JSON body
-    const jsonFormData = buildJsonFormData(form);
-    // 2.3 Build Headers
-    const headers = buildHeaders();
-    // 2.4 Request & Response
-    const response = await fetchService.performPostHttpRequest(`https://nd-rl-blog-api.onrender.com/signin`, headers, jsonFormData); // Uses JSON Placeholder
-    console.log(response);
-    // 2.5 Inform user of result
-    if(response)
-        window.location = `/success.html?FirstName=${response.FirstName}&LastName=${response.LastName}&Email=${response.Email}&id=${response.id}`;
-    else
-        alert(`An error occured.`);
+  // 1. Prevent reloading page
+  e.preventDefault();
+  // 2. Submit the form
+  // 2.1 User Interaction
+  const btnSubmit = document.getElementById("btnSubmit");
+  btnSubmit.disabled = true;
+  setTimeout(() => (btnSubmit.disabled = false), 2000);
+  // 2.2 Build JSON body
+  const jsonFormData = buildJsonFormData(form);
+
+  fetch("http://127.0.0.1:4000/signin", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jsonFormData),
+  })
+    .then((response) => response.json())
+    .then((jsonFormData) => {
+      var datas = JSON.parse(JSON.stringify(jsonFormData));
+      var token = datas.token;
+      document.cookie = `Bearer ${token}`;
+      console.log(document.cookie);
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
 }
 
 async function submitFormRegister(e, form) {
-    // 1. Prevent reloading page
-    e.preventDefault();
-    // 2. Submit the form
-    console.log('dans submitFormRegister')
-    // 2.1 User Interaction
-    const btnSubmit = document.getElementById('btnSubmit');
-    btnSubmit.disabled = true;
-    setTimeout(() => btnSubmit.disabled = false, 2000);
-    // 2.2 Build JSON body
-    const jsonFormData = buildJsonFormData(form);
+  // 1. Prevent reloading page
+  e.preventDefault();
+  // 2. Submit the form
+  // 2.1 User Interaction
+  const btnSubmit = document.getElementById("btnSubmit");
+  btnSubmit.disabled = true;
+  setTimeout(() => (btnSubmit.disabled = false), 2000);
+  // 2.2 Build JSON body
+  const jsonFormData = buildJsonFormData(form);
 
-
-
-    fetch('http://127.0.0.1:7000/signup', {
-        method:'POST',
-        headers: {
-            'Content-Type':'application/json'
-        },
-        body: jsonFormData
+  fetch("http://127.0.0.1:4000/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jsonFormData),
+  })
+    .then((response) => response.json())
+    .then((jsonFormData) => {
+      var datas = JSON.parse(JSON.stringify(jsonFormData));
+      var token = datas.token;
+      document.cookie = `Bearer ${token}`;
+      console.log(document.cookie);
     })
-    .then(response => response.json())
-    .then(jsonFormData => {
-        console.log('success', jsonFormData)
-    })
-    .catch((error)=> {
-        console.log('Error:', error)
-    })
-    // 2.3 Build Headers
-
-
-
-//     const headers = buildHeaders();
-
-
-
-//     // 2.4 Request & Response
-//     const response = await performPostHttpRequest(`https://nd-rl-blog-api.onrender.com/signup`, headers, jsonFormData); // Uses JSON Placeholder
-//     console.log(response);
-//     // 2.5 Inform user of result
-//     if(response)
-//         console.log('enregister', response)
-//        // window.location = `/success.html?FirstName=${response.FirstName}&LastName=${response.LastName}&Email=${response.Email}&id=${response.id}`;
-//     else
-//         alert(`An error occured.`);
-// }
+    .catch((error) => {
+      console.log("Error:", error);
+    });
 }
 
-function buildHeaders(authorization = null) {
-    const headers = {
-        "Content-Type": "application/json",
-        "Authorization": (authorization) ? authorization : "Bearer TOKEN_MISSING"
-    };
-    return headers;
+async function submitFormNewArticle(e, form) {
+  e.preventDefault();
+  const btnSubmit = document.getElementById("btnSubmit");
+  btnSubmit.disabled = true;
+  setTimeout(() => (btnSubmit.disabled = false), 2000);
+  // 2.2 Build JSON body
+  const jsonFormData = buildJsonFormData(form);
+  console.log(JSON.stringify(jsonFormData))
+  fetch("http//127.0.0.1:4000/api/post", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jsonFormData),
+  })
+    .then((response) => response.json())
+    .then((jsonFormData) => {
+console.log(jsonFormData)
+    })
+    .catch((error) => {
+        console.log("Error:", error);
+      });
 }
 
 function buildJsonFormData(form) {
-    const jsonFormData = { };
-    for(const pair of new FormData(form)) {
-        jsonFormData[pair[0]] = pair[1];
-    }
-    console.log(jsonFormData)
-    return jsonFormData;
-}
-/*--/Functions--*/
-
-async function performPostHttpRequest(fetchLink, headers, body) {
-    console.log('dans post')
-    if(!fetchLink || !headers || !body) {
-        throw new Error("One or more POST request parameters was not passed.");
-    }
-    try {
-        console.log('dans try post')
-        const rawResponse = await fetch(fetchLink, {
-            method: "POST",
-            //headers: headers,
-            body: JSON.stringify(body)
-        });
-        console.log(rawResponse)
-        const content = await rawResponse.json();
-        console.log('success')
-        return content;
-    }
-    catch(err) {
-        console.error(`Error at fetch POST: ${err}`);
-        throw err;
-    }
+  const jsonFormData = {};
+  for (const pair of new FormData(form)) {
+    jsonFormData[pair[0]] = pair[1];
+  }
+  return jsonFormData;
 }
 
 /*--Event Listeners--*/
 const connectForm = document.querySelector("#connectForm");
-if(connectForm) {
-    connectForm.addEventListener("submit", function(e) {
-        e.preventDefault()
-        console.log(this)
-        submitFormConnect(e, this);
-    });
-}
-const registerForm = document.querySelector("#registerForm");
-if(registerForm) {
-    registerForm.addEventListener("submit", function(e) {
-        submitFormRegister(e, this);
-    });
-}
+// if(connectForm) {
+//     connectForm.addEventListener("submit", function(e) {
+//         e.preventDefault()
+//         submitFormConnect(e, this);
+//     });
+// }
+// const registerForm = document.querySelector("#registerForm");
+// if(registerForm) {
+//     registerForm.addEventListener("submit", function(e) {
+//         submitFormRegister(e, this);
+//     });
+// }
+
+/**
+ * TODO:VERIFIER SI USER CONNECTER POUR LE BON MAIN
+ */
